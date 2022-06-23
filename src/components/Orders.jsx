68ef55de02fa9a3e75de6a717788fdbad3248e15
JangJinWon <wlsdnjs156@naver.com>
2022-06-23 23:51:55 +0900

    shopping mall project

---
+++ src/components/Orders.jsx
@@ -0,0 +1,84 @@
+import { useMemo } from "react";
+import useActions from "../hooks/useActions";
+import useOrders from "../hooks/useOrders";
+import usePrototypes from "../hooks/usePrototypes";
+
+export default function Orders() {
+  const orders = useOrders();
+  const prototypes = usePrototypes();
+  const { remove, removeAll } = useActions(); // 삭제 상태 관리
+
+  // total을 구해주는 상태관리
+  const totalPrice = useMemo(() => {
+    return orders.map(order => {
+      const {id, quantity} = order;
+      const prototype = prototypes.find(p => p.id === id)
+      return prototype.price * quantity;
+    }).reduce((l, r) => l + r, 0);
+  }, [orders, prototypes]);
+
+  if(orders.length === 0) { // orders(주문 목록)에 데이터가 없을 경우
+    return (
+      <aside>
+        <div className="empty">
+          <div className="title">You don't have any orders</div>    
+          <div className="subtitle">Click on a + to add an order</div>    
+        </div>
+      </aside>
+    );
+  }
+  // orders(주문 목록)에 데이터가 있는 경우
+  // item click 갯수와 가격 / 전체 토탈 가격 
+  return (
+  <aside> 
+    <div className="order">
+      <div className="body">
+
+        {orders.map(order => {
+          const { id } = order;
+          const prototype = prototypes.find((p) => p.id === id);
+          const click = () => {
+            remove(id);
+        }
+
+        return (
+          <div className="item" key={id}>
+            <div className="img">
+              <video src={prototype.thumbnail}/> 
+            </div>
+            <div className="content">
+              <p className="title">
+                {prototype.title} x {order.quantity}
+              </p>
+            </div>
+            <div className="action">
+              <p className="price">
+                $ {prototype.price * order.quantity}
+              </p>
+              <button className="btn--link" onClick={click}>
+              <i className="icon icon--cross" />
+              </button>
+            </div>
+          </div>              
+          );
+        })}
+      </div>    
+
+      <div className="total">
+        <hr />
+        <div className="item">
+          <div className="content">Total</div>
+          <div className="action">
+            <div className="price">$ {totalPrice}</div>
+          </div>
+          <button className="btn btn--link" onClick={removeAll}>
+            <i className="icon icon--delete" />
+          </button>
+          </div>
+        </div>
+
+        <button className="btn btn--secondary" style={{width: "100%", marginTop: 10}}>Ckeckout</button>
+    </div>
+  </aside> 
+  );
+}
